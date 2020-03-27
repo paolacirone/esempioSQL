5a77984575bd936cbd72b58169cf21d6200014d0
Lenovo Personal Pc <Lenovo Personal Pc@LAPTOP-M4HO8946.homenet.telecomitalia.it>
2020-03-27 17:21:09 +0100

    DAO

--- src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
+++ src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
@@ -1,56 +1,19 @@
 package it.polito.tdp.esempioSQL.model;
+import java.util.List;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import it.polito.tdp.esempioSQL.db.BabsDAO;
 
 public class LeggiBabs {
 
 	public void run() {
-
-		// 1. definire la stringa connessione, identifica i database a cui vogliamo
-		// collegarci
-		String jdbcURL = "jdbc:mysql://localhost/babs?user=root&password=root";
-
-		// 2. identificare la stringa di connessine per aprirla
-		try {
-			Connection conn = DriverManager.getConnection(jdbcURL);
-
-			// 3. eseguire un'interrogazione ES: conoscere i nomi delle stazioni
-            // 4.Query
-			String sql = "SELECT NAME FROM station WHERE landmark = ? " ;
-			PreparedStatement st = conn.prepareStatement(sql);
-
-			// si riferisce al ?
-			st.setString(1, "Palo Alto");
-			ResultSet res = st.executeQuery();
-			// next restituisce un boolean e mi dice se la riga esiste o no
-
-			while (res.next()) {
-
-				String nomeStazione = res.getString("name");
-				System.out.println(nomeStazione);
-
-			}
-
-			st.close();
-			// prima di chiudere potrei creare anche un altro statement
-
-			// 6.Chiudere la connessione
-			conn.close();
-
-		} catch (SQLException e) {
-
-			e.printStackTrace();
+		
+		BabsDAO dao = new BabsDAO();
+		
+		List<Station> tutte = dao.listStation();
+		
+		for(Station s : tutte) {
+			System.out.println(s.getName());
 		}
-
-		// FACTORY: creazione di un oggetto di una classe senza conoscerne il tipo
-		// (senza new) uso un metodo fornito da un'altra classe che internamente farà da
-		// new e conoscerà
-		// il tipo di classe effettivo
 
 	}
 
